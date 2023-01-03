5e0c3b9ae61d6e40ca138b7d0ec74a984b35bf1b
torres-developer <torres.dev@disroot.org>
2023-01-03 00:41:58 +0000

    starting

---
+++ search.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j/dbtype"
+)
+
+type Config struct {
+	Host     string `json:"host"`
+	Port     uint16 `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"passwd"`
+	Database string `json:"db"`
+}
+
+func (c Config) GetDbUri() string {
+	return fmt.Sprintf("neo4j://%s:%d", c.Host, c.Port)
+}
+
+type DriverProxy struct {
+	ctx    context.Context
+	driver neo4j.DriverWithContext
+	db     string
+}
+
+func (p DriverProxy) createSession(mode neo4j.AccessMode) neo4j.SessionWithContext {
+	return p.driver.NewSession(p.ctx, neo4j.SessionConfig{
+		AccessMode:   mode,
+		DatabaseName: p.db,
+	})
+}
+
+type Preference string
+
+func CreateConfig(filePath string) (conf Config, err error) {
+	var file *os.File
+
+	file, err = os.Open(filePath)
+	defer file.Close()
+
+	if err == nil {
+		err = json.NewDecoder(file).Decode(&conf)
+	}
+
+	return conf, err
+}
+
+func (proxy DriverProxy) GetPreferences() ([]*Preference, error) {
+	session := proxy.createSession(neo4j.AccessModeRead)
+
+	defer session.Close(proxy.ctx)
+
+	cypher, params := "MATCH (p:Preference) RETURN p", make(map[string]any)
+
+	res, err := session.Run(proxy.ctx, cypher, params)
+
+	if err != nil {
+		return make([]*Preference, 0), err
+	}
+
+	records, err := res.Collect(proxy.ctx)
+
+	if err != nil {
+		return make([]*Preference, 0), err
+	}
+
+	preferences := make([]*Preference, len(records))
+	for i, record := range records {
+		node, exists := record.Get("p")
+
+		if exists {
+			node, ok := node.(dbtype.Node)
+
+			if ok {
+				name := Preference(node.Props["name"].(string))
+				preferences[i] = &name
+			}
+		}
+	}
+
+	return preferences, err
+}
+
+func (proxy DriverProxy) NewPreference(preference Preference) error {
+	session := proxy.createSession(neo4j.AccessModeWrite)
+
+	defer session.Close(proxy.ctx)
+
+	cypher, params := "MERGE (:Preference {name: $p})", make(map[string]any)
+	params["p"] = strings.ToLower(string(preference))
+
+	_, err := session.ExecuteWrite(proxy.ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		return tx.Run(proxy.ctx, cypher, params)
+	})
+
+	if err != nil {
+		return err
+	}
+
+	cypher = `MATCH (a:Preference {name: $p}), (b:Preference)
+    WHERE NOT a = b
+    MERGE (a)-[:SHARES {value: 0}]->(b)`
+
+	_, err = session.ExecuteWrite(proxy.ctx, func(tx neo4j.ManagedTransaction) (any, error) {
+		return tx.Run(proxy.ctx, cypher, params)
+	})
+
+	return err
+}
+
+func main() {
+	conf, err := CreateConfig("credentials.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	auth := neo4j.BasicAuth(conf.User, conf.Password, "")
+
+	driver, err := neo4j.NewDriverWithContext(conf.GetDbUri(), auth)
+
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	defer driver.Close(ctx)
+
+	proxy := DriverProxy{ctx, driver, "neo4j"}
+
+	value, err := proxy.GetPreferences()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*value[0])
+}

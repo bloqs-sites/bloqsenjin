0ba303fc02b56572aa7b467f0341ea709f00639c
torres-developer <torres.dev@disroot.org>
2023-04-29 16:09:50 +0100

    env vars + links changes

--- internal/auth/server/server.go
+++ internal/auth/server/server.go
@@ -24,19 +24,25 @@ const (
 	cnf_env_var      = "BLOQS_CONF"
 
 	sch_flag         = "bloqs-schema"
-	sch_default_path = "https://black-silence-a2dc.torres-dev.workers.dev/"
+	sch_default_path = "https://bloqs.torres-dev.workers.dev/sch"
 	sch_usage        = ""
 	sch_env_var      = "BLOQS_SCHEMA"
 )
 
 func init() {
-    path := os.Getenv(cnf_env_var)
-    cnf_path = &path
-    flag.StringVar(cnf_path, cnf_flag, cnf_default_path, cnf_usage)
+	path, exists := os.LookupEnv(cnf_env_var)
+	if !exists {
+		flag.StringVar(cnf_path, cnf_flag, cnf_default_path, cnf_usage)
+	} else {
+		flag.StringVar(cnf_path, cnf_flag, path, cnf_usage)
+    }
 
-    path = os.Getenv(sch_env_var)
-    sch_path = &path
-    flag.StringVar(sch_path, sch_flag, sch_default_path, sch_usage)
+	path, exists = os.LookupEnv(sch_env_var)
+	if !exists {
+		flag.StringVar(sch_path, sch_flag, sch_default_path, sch_usage)
+	} else {
+		flag.StringVar(sch_path, sch_flag, path, sch_usage)
+    }
 }
 
 func Server() http.HandlerFunc {
